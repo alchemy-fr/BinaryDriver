cc49730df03655a9a905d87d3fae53d77b7e03a6
Romain Neutron <imprec@gmail.com>
2013-04-24 10:00:04 +0200

    Add license doc blocks

--- src/Alchemy/BinaryDriver/ConfigurationInterface.php
+++ src/Alchemy/BinaryDriver/ConfigurationInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Alchemy\BinaryDriver.
+ *
+ * (c) Alchemy <info@alchemy.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Alchemy\BinaryDriver;
 
 use Alchemy\BinaryDriver\Exception\InvalidArgumentException;
