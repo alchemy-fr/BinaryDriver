3dd057841bc0ffe2c5d43086e6098920a00d423f
Romain Neutron <imprec@gmail.com>
2013-04-26 12:53:27 +0200

    Add BinaryDriver::createProcessMock method

--- src/Alchemy/BinaryDriver/BinaryDriverTestCase.php
+++ src/Alchemy/BinaryDriver/BinaryDriverTestCase.php
@@ -3,6 +3,7 @@
 namespace Alchemy\BinaryDriver;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Process\Process;
 
 /**
  * Convenient PHPUnit methods for testing BinaryDriverInterface implementations.
@@ -15,6 +16,42 @@ class BinaryDriverTestCase extends \PHPUnit_Framework_TestCase
     public function createProcessBuilderFactoryMock()
     {
         return $this->getMock('Alchemy\BinaryDriver\ProcessBuilderFactoryInterface');
+    }
+
+    /**
+     * @param integer $runs        The number of runs expected
+     * @param Boolean $success     True if the process expects to be successfull
+     * @param string  $commandLine The commandline executed
+     * @param string  $output      The process output
+     * @param string  $error       The process error output
+     *
+     * @return Process
+     */
+    public function createProcessMock($runs = 1, $success = true, $commandLine = null, $output = null, $error = null)
+    {
+        $process = $this->getMockBuilder('Symfony\Component\Process\Process')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $process->expects($this->exactly($runs))
+            ->method('run');
+
+        $process->expects($this->any())
+            ->method('isSuccessful')
+            ->will($this->returnValue($success));
+
+        foreach (array(
+            'getOutput' => $output,
+            'getErrorOutput' => $error,
+            'getCommandLine' => $commandLine,
+        ) as $command => $value) {
+            $process
+                ->expects($this->any())
+                ->method($command)
+                ->will($this->returnValue($value));
+        }
+
+        return $process;
     }
 
     /**
